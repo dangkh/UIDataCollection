eb03a9f08bccee05c4ff92dc526fe4d6444a8c45
dangkh <dangkh109@gmail.com>
2020-11-17 16:17:45 +0700

    Add files via upload

---
+++ ReadData.py
@@ -0,0 +1,16 @@
+import numpy as np
+import json
+import os
+
+def readData(link = "./DataVIN/", prefixName = "Data"):
+	if os.path.isdir(link):
+		onlyfiles = [f for f in os.listdir(link) if os.path.isfile(link+f)]
+		jsonFiles = [link+x for x in onlyfiles if x[-4:] == 'json']
+		print(jsonFiles)
+	else:
+		print("imported link is not exist")
+		return []
+	return jsonFiles
+
+if __name__ == "__main__":
+	readData()
