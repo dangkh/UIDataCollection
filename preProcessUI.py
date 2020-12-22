7b43869fb928dae9da57e57d282fb358511ed843
Hai Dang Kieu <dangkh109@gmail.com>
2020-12-22 16:54:12 +0700

    add choosing option

--- preProcessUI.py
+++ preProcessUI.py
@@ -11,14 +11,25 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 from PyQt5 import *
-from PyQt5.QtWidgets  import *
+from PyQt5.QtWidgets import *
 import json
-import random
-import os 
-import math
-import _thread
-import time
-from datetime import datetime
+import numpy
+
+
+class textSpinBox(QSpinBox):
+    def __init__(self, parent=None):
+        super(textSpinBox, self).__init__(parent)
+        self.set_list_string()
+
+    def set_list_string(self, strings=None):
+        if strings is not None:
+            self.list_text = strings
+        else:
+            self.list_text = ["EEG", "ET"]
+        self.setRange(0, len(self.list_text) - 1)
+
+    def textFromValue(self, value):
+        return self.list_text[value]
 
 
 class Ui_MainWindow(object):
@@ -26,7 +37,8 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1195, 711)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("my-beautiful-life-logo-3.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("my-beautiful-life-logo-3.png"),
+                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
@@ -43,43 +55,49 @@ class Ui_MainWindow(object):
         self.formLayout_2.setObjectName("formLayout_2")
         self.label_5 = QtWidgets.QLabel(self.formLayoutWidget_2)
         self.label_5.setObjectName("label_5")
-        self.formLayout_2.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_5)
+        self.formLayout_2.setWidget(
+            1, QtWidgets.QFormLayout.LabelRole, self.label_5)
         self.spinBox_2 = QtWidgets.QSpinBox(self.formLayoutWidget_2)
         self.spinBox_2.setInputMethodHints(QtCore.Qt.ImhLowercaseOnly)
         self.spinBox_2.setObjectName("spinBox_2")
-        self.formLayout_2.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.spinBox_2)
+        self.formLayout_2.setWidget(
+            1, QtWidgets.QFormLayout.FieldRole, self.spinBox_2)
         self.label_6 = QtWidgets.QLabel(self.formLayoutWidget_2)
         self.label_6.setObjectName("label_6")
-        self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_6)
+        self.formLayout_2.setWidget(
+            2, QtWidgets.QFormLayout.LabelRole, self.label_6)
         self.doubleSpinBox = QtWidgets.QDoubleSpinBox(self.formLayoutWidget_2)
         self.doubleSpinBox.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
         self.doubleSpinBox.setInputMethodHints(QtCore.Qt.ImhDigitsOnly)
         self.doubleSpinBox.setObjectName("doubleSpinBox")
-        self.formLayout_2.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.doubleSpinBox)
+        self.formLayout_2.setWidget(
+            2, QtWidgets.QFormLayout.FieldRole, self.doubleSpinBox)
         self.label_2 = QtWidgets.QLabel(self.formLayoutWidget_2)
         self.label_2.setObjectName("label_2")
-        self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_2)
-        self.spinBox = QtWidgets.QSpinBox(self.formLayoutWidget_2)
+        self.formLayout_2.setWidget(
+            0, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.spinBox = textSpinBox(self.formLayoutWidget_2)
         self.spinBox.setObjectName("spinBox")
-        self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.spinBox)
+        self.formLayout_2.setWidget(
+            0, QtWidgets.QFormLayout.FieldRole, self.spinBox)
         self.pushButton = QtWidgets.QPushButton(self.groupBox)
         self.pushButton.setGeometry(QtCore.QRect(118, 584, 211, 41))
         self.pushButton.setWhatsThis("")
         self.pushButton.setStyleSheet("QPushButton#pushButton {\n"
-"    background-color: rgb(167, 192, 220);\n"
-"    border-style: outset;\n"
-"    border-width: 2px;\n"
-"    border-radius: 10px;\n"
-"    border-color: beige;\n"
-"    font: bold 14px;\n"
-"    min-width: 10em;\n"
-"    padding: 6px;\n"
-"}\n"
-"\n"
-"QPushButton#pushButton:pressed {\n"
-"    background-color: rgb(171, 196, 223);\n"
-"    border-style: inset;\n"
-"}")
+                                      "    background-color: rgb(167, 192, 220);\n"
+                                      "    border-style: outset;\n"
+                                      "    border-width: 2px;\n"
+                                      "    border-radius: 10px;\n"
+                                      "    border-color: beige;\n"
+                                      "    font: bold 14px;\n"
+                                      "    min-width: 10em;\n"
+                                      "    padding: 6px;\n"
+                                      "}\n"
+                                      "\n"
+                                      "QPushButton#pushButton:pressed {\n"
+                                      "    background-color: rgb(171, 196, 223);\n"
+                                      "    border-style: inset;\n"
+                                      "}")
         self.pushButton.setObjectName("pushButton")
         self.formLayoutWidget_3 = QtWidgets.QWidget(self.groupBox)
         self.formLayoutWidget_3.setGeometry(QtCore.QRect(0, 40, 431, 391))
@@ -89,16 +107,20 @@ class Ui_MainWindow(object):
         self.formLayout_3.setObjectName("formLayout_3")
         self.NameEdit = QtWidgets.QLineEdit(self.formLayoutWidget_3)
         self.NameEdit.setObjectName("NameEdit")
-        self.formLayout_3.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.NameEdit)
+        self.formLayout_3.setWidget(
+            0, QtWidgets.QFormLayout.FieldRole, self.NameEdit)
         self.label_8 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_8.setObjectName("label_8")
-        self.formLayout_3.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_8)
+        self.formLayout_3.setWidget(
+            1, QtWidgets.QFormLayout.LabelRole, self.label_8)
         self.AgeEdit = QtWidgets.QSpinBox(self.formLayoutWidget_3)
         self.AgeEdit.setObjectName("AgeEdit")
-        self.formLayout_3.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.AgeEdit)
+        self.formLayout_3.setWidget(
+            1, QtWidgets.QFormLayout.FieldRole, self.AgeEdit)
         self.label_9 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_9.setObjectName("label_9")
-        self.formLayout_3.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_9)
+        self.formLayout_3.setWidget(
+            2, QtWidgets.QFormLayout.LabelRole, self.label_9)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
         self.MaleEdit = QtWidgets.QRadioButton(self.formLayoutWidget_3)
@@ -107,48 +129,64 @@ class Ui_MainWindow(object):
         self.FemaleEdit = QtWidgets.QRadioButton(self.formLayoutWidget_3)
         self.FemaleEdit.setObjectName("FemaleEdit")
         self.horizontalLayout_2.addWidget(self.FemaleEdit)
-        self.formLayout_3.setLayout(2, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_2)
+        self.formLayout_3.setLayout(
+            2, QtWidgets.QFormLayout.FieldRole, self.horizontalLayout_2)
         self.label_14 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_14.setObjectName("label_14")
-        self.formLayout_3.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_14)
+        self.formLayout_3.setWidget(
+            3, QtWidgets.QFormLayout.LabelRole, self.label_14)
         self.DiseaseDescEdit = QtWidgets.QTextEdit(self.formLayoutWidget_3)
         self.DiseaseDescEdit.setObjectName("DiseaseDescEdit_2")
-        self.formLayout_3.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.DiseaseDescEdit)
-        spacerItem = QtWidgets.QSpacerItem(20, 89, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.formLayout_3.setItem(4, QtWidgets.QFormLayout.FieldRole, spacerItem)
+        self.formLayout_3.setWidget(
+            3, QtWidgets.QFormLayout.FieldRole, self.DiseaseDescEdit)
+        spacerItem = QtWidgets.QSpacerItem(
+            20, 89, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.formLayout_3.setItem(
+            4, QtWidgets.QFormLayout.FieldRole, spacerItem)
         self.label_10 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_10.setObjectName("label_10")
-        self.formLayout_3.setWidget(5, QtWidgets.QFormLayout.LabelRole, self.label_10)
+        self.formLayout_3.setWidget(
+            5, QtWidgets.QFormLayout.LabelRole, self.label_10)
         self.RecorderEdit = QtWidgets.QLineEdit(self.formLayoutWidget_3)
         self.RecorderEdit.setObjectName("RecorderEdit")
-        self.formLayout_3.setWidget(5, QtWidgets.QFormLayout.FieldRole, self.RecorderEdit)
+        self.formLayout_3.setWidget(
+            5, QtWidgets.QFormLayout.FieldRole, self.RecorderEdit)
         self.label_11 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_11.setObjectName("label_11")
-        self.formLayout_3.setWidget(6, QtWidgets.QFormLayout.LabelRole, self.label_11)
+        self.formLayout_3.setWidget(
+            6, QtWidgets.QFormLayout.LabelRole, self.label_11)
         self.LocateEdit = QtWidgets.QLineEdit(self.formLayoutWidget_3)
         self.LocateEdit.setObjectName("LocateEdit")
-        self.formLayout_3.setWidget(6, QtWidgets.QFormLayout.FieldRole, self.LocateEdit)
+        self.formLayout_3.setWidget(
+            6, QtWidgets.QFormLayout.FieldRole, self.LocateEdit)
         self.label_12 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_12.setObjectName("label_12")
-        self.formLayout_3.setWidget(7, QtWidgets.QFormLayout.LabelRole, self.label_12)
+        self.formLayout_3.setWidget(
+            7, QtWidgets.QFormLayout.LabelRole, self.label_12)
         self.RecPlanEdit = QtWidgets.QSpinBox(self.formLayoutWidget_3)
         self.RecPlanEdit.setObjectName("RecPlanEdit")
-        self.formLayout_3.setWidget(7, QtWidgets.QFormLayout.FieldRole, self.RecPlanEdit)
+        self.formLayout_3.setWidget(
+            7, QtWidgets.QFormLayout.FieldRole, self.RecPlanEdit)
         self.label_21 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_21.setObjectName("label_21")
-        self.formLayout_3.setWidget(8, QtWidgets.QFormLayout.LabelRole, self.label_21)
+        self.formLayout_3.setWidget(
+            8, QtWidgets.QFormLayout.LabelRole, self.label_21)
         self.sentenceIdEdit = QtWidgets.QSpinBox(self.formLayoutWidget_3)
         self.sentenceIdEdit.setObjectName("sentenceIdEdit")
-        self.formLayout_3.setWidget(8, QtWidgets.QFormLayout.FieldRole, self.sentenceIdEdit)
+        self.formLayout_3.setWidget(
+            8, QtWidgets.QFormLayout.FieldRole, self.sentenceIdEdit)
         self.label_13 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_13.setObjectName("label_13")
-        self.formLayout_3.setWidget(9, QtWidgets.QFormLayout.LabelRole, self.label_13)
+        self.formLayout_3.setWidget(
+            9, QtWidgets.QFormLayout.LabelRole, self.label_13)
         self.dateTimeEdit = QtWidgets.QDateTimeEdit(self.formLayoutWidget_3)
         self.dateTimeEdit.setObjectName("dateTimeEdit")
-        self.formLayout_3.setWidget(9, QtWidgets.QFormLayout.FieldRole, self.dateTimeEdit)
+        self.formLayout_3.setWidget(
+            9, QtWidgets.QFormLayout.FieldRole, self.dateTimeEdit)
         self.label_7 = QtWidgets.QLabel(self.formLayoutWidget_3)
         self.label_7.setObjectName("label_7")
-        self.formLayout_3.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_7)
+        self.formLayout_3.setWidget(
+            0, QtWidgets.QFormLayout.LabelRole, self.label_7)
         self.label = QtWidgets.QLabel(self.groupBox)
         self.label.setGeometry(QtCore.QRect(180, 10, 101, 21))
         font = QtGui.QFont()
@@ -190,46 +228,6 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         self.createEvent()
 
-
-    def createEvent(self):
-        self.actionExit.setShortcut("Ctrl+Q")
-        self.actionExit.triggered.connect(self.closeApp)
-        self.actionOpen_file.setShortcut("Ctrl+O")
-        self.actionOpen_file.setStatusTip('Open file browser')
-        self.actionOpen_file.triggered.connect(self.openFilePath)
-
-    def openFilePath(self):
-        fname = QFileDialog.getOpenFileName(filter = "Json (*.json)")
-        path = fname[0]
-        
-        print(path)
-        with open(path) as json_file:
-            data = json.load(json_file)
-        
-        # self.viewData = data
-
-        self.NameEdit.setText(data['name'])
-        self.AgeEdit.setValue(data['age'])
-        if data['gender'] == 'M':
-            self.MaleEdit.setChecked(True)
-        else:
-            self.FemaleEdit.setChecked(True)
-        self.DiseaseDescEdit.setText(data['patientDesc'])
-        self.RecorderEdit.setText(data['recorder'])
-        self.LocateEdit.setText(data['location'])
-        self.RecPlanEdit.setValue(data['recPlan'])
-        self.sentenceIdEdit.setValue(data['sentenceID'])
-        dataTime = QtCore.QDateTime.fromString(data['time'], 'd/M/yyyy hh:mm')
-        self.dateTimeEdit.setDateTime(dataTime)
-        listEditatble = [self.NameEdit, self.AgeEdit, self.MaleEdit, self.FemaleEdit, self.DiseaseDescEdit, 
-            self.RecorderEdit, self.LocateEdit, self.RecPlanEdit, self.sentenceIdEdit, self.dateTimeEdit]
-        for x in listEditatble:
-            x.setEnabled(False)
-
-
-    def closeApp(self):
-        sys.exit()
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
@@ -255,9 +253,46 @@ class Ui_MainWindow(object):
         self.actionSave_as.setText(_translate("MainWindow", "Save as "))
         self.actionExit.setText(_translate("MainWindow", "Exit"))
 
+    def createEvent(self):
+        self.actionExit.setShortcut("Ctrl+Q")
+        self.actionExit.triggered.connect(self.closeApp)
+        self.actionOpen_file.setShortcut("Ctrl+O")
+        self.actionOpen_file.setStatusTip('Open file browser')
+        self.actionOpen_file.triggered.connect(self.openFilePath)
+
+    def openFilePath(self):
+        fname = QFileDialog.getOpenFileName(filter="Json (*.json)")
+        path = fname[0]
+
+        print(path)
+        with open(path) as json_file:
+            data = json.load(json_file)
+
+        # self.viewData = data
+
+        self.NameEdit.setText(data['name'])
+        self.AgeEdit.setValue(data['age'])
+        if data['gender'] == 'M':
+            self.MaleEdit.setChecked(True)
+        else:
+            self.FemaleEdit.setChecked(True)
+        self.DiseaseDescEdit.setText(data['patientDesc'])
+        self.RecorderEdit.setText(data['recorder'])
+        self.LocateEdit.setText(data['location'])
+        self.RecPlanEdit.setValue(data['recPlan'])
+        self.sentenceIdEdit.setValue(data['sentenceID'])
+        dataTime = QtCore.QDateTime.fromString(data['time'], 'd/M/yyyy hh:mm')
+        self.dateTimeEdit.setDateTime(dataTime)
+        listEditatble = [self.NameEdit, self.AgeEdit, self.MaleEdit, self.FemaleEdit, self.DiseaseDescEdit,
+                         self.RecorderEdit, self.LocateEdit, self.RecPlanEdit, self.sentenceIdEdit, self.dateTimeEdit]
+        for x in listEditatble:
+            x.setEnabled(False)
+
+    def closeApp(self):
+        sys.exit()
+
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
